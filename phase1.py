febe331e0682b411e670c283cb615d9b44b4058f
Amanda Nguyen <amandanguyen@Amandas-MacBook-Pro.local>
2019-11-20 21:00:52 -0700

    ignore this lol

--- phase1.py
+++ phase1.py
@@ -1,9 +1,9 @@
 import re 
 
 xml_content = open('test10.xml').read()
-terms_file = open('terms.txt','w')
+terms_file = open('terms.txt','w') # creating the terms file
 
 row_numbers = re.findall('<row>(.*)</row>', xml_content)
 for row in row_numbers:
     print(row)
-    subject_terms = re.findall('<subj>(.*)</subj>',)
+    subject_terms = re.findall('<subj>(.*)</subj>',xml_content)
