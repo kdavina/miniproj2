cc1f0473badfab827e5495b040c3b8db109b9c5f
kdavina <37858141+kdavina@users.noreply.github.com>
2019-11-20 20:50:39 -0700

    TERMS.TXT LET'S GO HOMIES

--- phase1.py
+++ phase1.py
@@ -4,6 +4,19 @@ xml_content = open('test10.xml').read()
 terms_file = open('terms.txt','w')
 
 row_numbers = re.findall('<row>(.*)</row>', xml_content)
-for row in row_numbers:
-    print(row)
-    subject_terms = re.findall('<subj>(.*)</subj>',)
+subjects = re.findall('<subj>(.*)</subj>', xml_content)
+bodies = re.findall('<body>(.*)</body>', xml_content)
+
+for row_index in range(len(row_numbers)):
+    split_subject = re.split('[^0-9a-zA-Z_-]',subjects[row_index])
+    split_body = re.split('[^0-9a-zA-Z_-]', bodies[row_index])
+    for term in split_subject:
+        if len(term)>2:
+            terms_file.write('s-' + term.lower() + ':' + row_numbers[row_index] + '\n')
+    for body in split_body:
+        if len(body)>2:
+            terms_file.write('b-' + body.lower() + ':' + row_numbers[row_index] + '\n')
+
+
+terms_file.close()
+    
